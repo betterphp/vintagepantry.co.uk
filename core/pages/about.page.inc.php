43f8368a4c9fa60b6ac759b66329713c20014c54
Jacek <jacek@kuzemczak.co.uk>
2013-10-11 15:50:01 +0100

    Improved about page.

--- core/pages/about.page.inc.php
+++ core/pages/about.page.inc.php
@@ -1,11 +1,11 @@
-<div id="about-image-container">
-	<img src="ext/img/about_1.jpg" alt="" />
-	<img src="ext/img/about_2.jpg" alt="" />
-	<img src="ext/img/about_3.jpg" alt="" />
+<div id="about-image"></div>
+
+<div id="about-text">
+	<p>The Vintage Pantry is an online store offering customers the ‘perfect’ vintage. Genuine vintage and retro kitchenware and homewares from across the decades are carefully sourced and hand-picked by us for their high quality, excellent condition, and style value.</p>
+	
+	<p>Alongside our pre-loved vintage pieces sit thoughtfully selected vintage-style new products from leading suppliers to style your home vintage.  Thank you for visiting us – we hope you find something special on our shelves!</p>
+	
+	<p>Tanya</p>
+	
+	<p>The Vintage Pantry</p>
 </div>
-
-<p>The Vintage Pantry is an online store offering customers the ‘perfect’ vintage. Genuine vintage and retro kitchenware and homewares from across the decades are carefully sourced and hand-picked by us for their high quality, excellent condition, and style value.  Alongside our pre-loved vintage pieces sit thoughtfully selected vintage-style new products from leading suppliers to style your home vintage.  Thank you for visiting us – we hope you find something special on our shelves!</p>
-
-<p>Tanya</p>
-
-<p>The Vintage Pantry</p>
