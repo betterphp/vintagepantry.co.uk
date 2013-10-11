1a8956462773d054fdd31d578a89b1d7a5b4915d
Jacek <jacek@kuzemczak.co.uk>
2013-10-11 16:38:34 +0100

    Improved formatting of about page text.

--- core/pages/about.page.inc.php
+++ core/pages/about.page.inc.php
@@ -1,11 +1,17 @@
 <div id="about-image"></div>
 
 <div id="about-text">
-	<p>The Vintage Pantry is an online store offering customers the ‘perfect’ vintage. Genuine vintage and retro kitchenware and homewares from across the decades are carefully sourced and hand-picked by us for their high quality, excellent condition, and style value.</p>
+	<p class="handwritten large title">Hello</p>
 	
-	<p>Alongside our pre-loved vintage pieces sit thoughtfully selected vintage-style new products from leading suppliers to style your home vintage.  Thank you for visiting us – we hope you find something special on our shelves!</p>
+	<p>Here at The Vintage Pantry we offer customers the perfect vintage.</p>
 	
-	<p>Tanya</p>
+	<p>Genuine vintage and retro kitchenware and homewares from across the decades are carefully sourced and hand-picked by us for their high quality, excellent condition, and style value.</p>
 	
-	<p>The Vintage Pantry</p>
+	<p>Alongside our pre-loved vintage pieces sit thoughtfully selected vintage-style new products from leading suppliers to style your home vintage.</p>
+	
+	<p>Thank you for visiting us – we hope you find something special on our shelves!</p>
+	
+	<p class="handwritten large signature">Tanya</p>
+	
+	<p class="handwritten">The Vintage Pantry</p>
 </div>
